b70d2ce52894c3dd4264f3d9515294a81354aa93
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-06-29 02:27:23 +0000

    traffic light made by cesaramcolson

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const initialColors = ["red", "yellow", "green"];
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [colors, setColors] = useState(initialColors);
+    const [color, setColor] = useState("red");
+
+    const cycleColor = () => {
+        const currentIndex = colors.indexOf(color);
+        const nextIndex = (currentIndex + 1) % colors.length;
+        setColor(colors[nextIndex]);
+    };
+
+    const addPurple = () => {
+        if (!colors.includes("purple")) {
+            setColors([...colors, "purple"]);
+        }
+    };
+
+    return (
+        <div className="container">
+            <div className="pole"></div>
+            <div className="traffic-light">
+                <div onClick={() => setColor("red")} className={"light red" + (color === "red" ? " glow-red" : "")}></div>
+                <div onClick={() => setColor("yellow")} className={"light yellow" + (color === "yellow" ? " glow-yellow" : "")}></div>
+                <div onClick={() => setColor("green")} className={"light green" + (color === "green" ? " glow-green" : "")}></div>
+                {colors.includes("purple") && (
+                    <div onClick={() => setColor("purple")} className={"light purple" + (color === "purple" ? " glow-purple" : "")}></div>
+                )}
+            </div>
+            <div>
+                <button className="btn btn-success mt-3" onClick={cycleColor}>Change Color</button>
+                <button className="btn btn-secondary mt-3 ms-2" onClick={addPurple}>Add Purple</button>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
